0da00c0ca07656325660f12a0f91fee725f00754
Vman1907 <varshmaan.sonkar@gmail.com>
2021-10-01 23:22:50 +0530

    final commit

--- Exam Website/Addon.js
+++ Exam Website/Addon.js
@@ -3,11 +3,12 @@ import "./Addon.css"
 
 export default function Addon(){
     return(
-        <div className=" container row">
-            <div className="col-5">
+        <div className="container">
+        <div className="row">
+            <div className="col-lg-5 col-sm-12">
                 <img src={Image} alt=""/>
             </div>
-            <div className="col-6">
+            <div className="col-lg-6 col-sm-12">
                 <ul>
                     <li>No. 1 Website For Fresher</li>
                     <li>No Registration charge</li>
@@ -19,6 +20,7 @@ export default function Addon(){
                     <li>Diploma program</li>
                 </ul>
             </div>
+        </div>
         </div>
     )
 }
